fdf589af0703964b609190e4e6810cd07bc42366
gitforschool <edrick@ualberta.ca>
2015-03-10 16:07:20 -0600

    my first commit

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -10,6 +10,10 @@ import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.JSpinner;
+import java.awt.FlowLayout;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class MainMenu extends JFrame {
 
@@ -36,15 +40,28 @@ public class MainMenu extends JFrame {
 	 */
 	public MainMenu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 777, 498);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		
-		JTextPane txtpnMainMenu = new JTextPane();
-		txtpnMainMenu.setText("Main Menu");
-		contentPane.add(txtpnMainMenu, BorderLayout.NORTH);
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		contentPane.add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) panel_1.getLayout();
+		flowLayout_1.setVgap(20);
+		flowLayout_1.setHgap(20);
+		contentPane.add(panel_1);
+		
+		JButton btnNewButton = new JButton("Test 2");
+		contentPane.add(btnNewButton);
+		btnNewButton.setSize(20, 20);
+		
+		JButton btnTest = new JButton("Test");
+		contentPane.add(btnTest);
 	}
 
 }
