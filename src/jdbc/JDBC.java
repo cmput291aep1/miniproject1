bd273eb8f7da495ea6aa705c620c50821d606d4a
gitforschool <edrick@ualberta.ca>
2015-03-12 15:02:24 -0600

    started JDBC tests

--- src/jdbc/JDBC.java
+++ src/jdbc/JDBC.java
@@ -1,5 +1,68 @@
 package jdbc;
 
-public class JDBC {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC
+{
+
+	/**
+	 * @param args
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws SQLException 
+	 */
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException
+	{
+		String m_driverName = "oracle.jdbc.driver.OracleDriver";
+
+		Class drvClass = Class.forName(m_driverName);
+		//DriverManager.registerDriver((Driver)drvClass.newInstance());
+
+		// Establish Connection
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS", "edrick", "cwielkisbl7");
+		System.out.println("Connection Created");
+
+		// Creating a statement
+		Statement stmt = con.createStatement();
+		String query = "select name from people";
+		// Result
+		ResultSet rs = stmt.executeQuery(query);
+
+		// Retrieve Data
+		while(rs.next()) {
+			System.out.println(rs.getString("Name"));
+		}
+
+	}
+
+	public void connect(String m_driverName, String m_url, String username,
+			String password)
+	{
+		try
+		{
+			Class drvClass = Class.forName(m_driverName);
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Establish Connection
+		try
+		{
+			Connection con = DriverManager.getConnection(m_url, username, password);
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Connection Created");
+
+	}
 
 }
--- src/miniproject1test/JDBCTest.java
+++ src/miniproject1test/JDBCTest.java
@@ -1,6 +1,7 @@
 package miniproject1test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+import jdbc.JDBC;
 
 import org.junit.Test;
 
@@ -9,6 +10,49 @@ public class JDBCTest {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+//	Search Engine
+//
+//	This component is used to perform the following searches.
+//
+	
+/*	List the name, licence_no, addr, birthday, driving class, driving_condition, and the expiring_data of a driver 
+ *	by entering either a licence_no or a given name. It shall display all the entries if a duplicate name is given.
+ *	"select p.name, d.licence_no, p.addr, p.birthday, d.class, dc.description, d.expiring_date
+ *   from people p, drive_licence d, driving_condition dc
+ *   where p.sin=d.sin and  
+ */
+//	List all violation records received by a person if  the drive licence_no or sin of a person  is entered.
+	
+/*	Print out the vehicle_history, including the number of times that a vehicle has been changed hand, the average price, 
+ * and the number of violations it has been involved by entering the vehicle's serial number.
+ */
+
+	
+	// Use Case: Database User Connects to the Database
+	@Test
+	public void testConnection() {
+		JDBC jdbcManager = new JDBC();
+		String m_driverName = "oracle.jdbc.driver.OracleDriver";
+		String m_url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+		String username = "edrick";
+		String password = "cwielkisbl7";
+		jdbcManager.connect(m_driverName, m_url, username, password);
+	}
+	
+	// Use Case: Database User queries the database
+	@Test
+	public void testQueryLicenseNum() {
+		JDBC jdbcMgr = new JDBC();
+		String m_driverName = "oracle.jdbc.driver.OracleDriver";
+		String m_url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+		String username = "edrick";
+		String password = "cwielkisbl7";
+		jdbcMgr.connect(m_driverName, m_url, username, password);
+		
+		// Query 
+		//String query = "select * from People"
 	}
 
 }
